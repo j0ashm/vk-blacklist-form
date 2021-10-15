2127f3557e09160e4c3f92aa3c86b4f1f1883c89
JustinOleskii <joashm14@gmail.com>
2021-10-15 17:24:39 +0530

    resolve cors problems

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
@@ -9,6 +10,7 @@ mongoose.Promise = global.Promise
 
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cors())
 
 app.use('/api', require('./Routes'))
 
