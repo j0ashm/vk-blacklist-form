fdb288af81486c3b18b08ff70e3504fc895ec926
JustinOleskii <joashm14@gmail.com>
2021-10-15 16:57:48 +0530

    create more api routes for convenience

--- backend/Routes.js
+++ backend/Routes.js
@@ -2,9 +2,80 @@ const express = require('express')
 const router = express.Router()
 const Blacklist = require('./Model')
 
-router.post('/create', (req, res) => {
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+
+var mailer = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+    }
+})
+
+router.post('/create/gar', (req, res) => {
     Blacklist.create(req.body).then((bl) => {
-        console.log(bl)
+        const { submitterUsername, toBLUsername, profileLink, discordID, blReason, division, duration, status } = bl
+        let mailBody = `**[${division}] Blacklist**\n\n________\n**Username:** ${toBLUsername}\n**ROBLOX Profile Link:** ${profileLink}\n**Discord ID:** ${discordID}\n________\n**Reason:** ${blReason}\n**Duration:** ${duration}\n**Status:** ${status}\n**Assigned by:** ${submitterUsername}`
+
+        mailer.sendMail({
+            from: process.env.GMAIL_USER,
+            to: process.env.GAR_EMAIL,
+            subject: toBLUsername,
+            text: mailBody
+        }, (err, info) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(`Email successfully sent!`, info.response)
+            }
+        })
+        res.status(200).send('Success!')
+    }).catch((err) => {
+        console.error(err)
+    })
+})
+
+router.post('/create/tsu', (req, res) => {
+    Blacklist.create(req.body).then((bl) => {
+        const { submitterUsername, toBLUsername, profileLink, discordID, blReason, division, duration, status } = bl
+        let mailBody = `**[${division}] Blacklist**\n\n________\n**Username:** ${toBLUsername}\n**ROBLOX Profile Link:** ${profileLink}\n**Discord ID:** ${discordID}\n________\n**Reason:** ${blReason}\n**Duration:** ${duration}\n**Status:** ${status}\n**Assigned by:** ${submitterUsername}`
+
+        mailer.sendMail({
+            from: process.env.GMAIL_USER,
+            to: process.env.TSU_EMAIL,
+            subject: toBLUsername,
+            text: mailBody
+        }, (err, info) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(`Email successfully sent!`, info.response)
+            }
+        })
+        res.status(200).send('Success!')
+    }).catch((err) => {
+        console.error(err)
+    })
+})
+
+router.post('/create/hre', (req, res) => {
+    Blacklist.create(req.body).then((bl) => {
+        const { submitterUsername, toBLUsername, profileLink, discordID, blReason, division, duration, status } = bl
+        let mailBody = `**[${division}] Blacklist**\n\n________\n**Username:** ${toBLUsername}\n**ROBLOX Profile Link:** ${profileLink}\n**Discord ID:** ${discordID}\n________\n**Reason:** ${blReason}\n**Duration:** ${duration}\n**Status:** ${status}\n**Assigned by:** ${submitterUsername}`
+
+        mailer.sendMail({
+            from: process.env.GMAIL_USER,
+            to: process.env.HRE_EMAIL,
+            subject: toBLUsername,
+            text: mailBody
+        }, (err, info) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(`Email successfully sent!`, info.response)
+            }
+        })
         res.status(200).send('Success!')
     }).catch((err) => {
         console.error(err)
